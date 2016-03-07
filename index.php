9d32a9dd014a95ca858315df8ce0a16bbe0e087c
LennartEdberg <lennartedb@hotmail.se>
2016-03-07 15:04:04 +0100

    La till så du kan använda enter när du skickar messages

--- index.php
+++ index.php
@@ -195,6 +195,11 @@ if(!isset($_SESSION['steamid'])) {
         
         $('.friendCell').on('click', startChat);
         $('#chatBtn').on('click', sendChatMsg);
+        $('#chatTxtInput').on( "keydown", function(event) {
+            if(event.which == 13) {
+                sendChatMsg();
+            }
+        });
 
         $('.friendCell').on('click', function(){
             if(!$('#chat').is(":visible")) {
